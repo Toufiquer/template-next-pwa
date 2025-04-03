3896816fb79db13f0098028b38e06b0913d1ccb9
Toufiquer <toufiquerabc@gmail.com>
2025-04-04 02:10:56 +0600

    done @ view modal

--- src/app/users-zustand/components/ViewUser.tsx
+++ src/app/users-zustand/components/ViewUser.tsx
@@ -1,36 +1,17 @@
 import React from 'react';
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
-import { Button } from '@/components/ui/button';
 import { format } from 'date-fns';
-import { IUser } from '@/app/api/v1/users/userModel';
+
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+
 import { useUserStore } from '../store/userStore';
 
 const ViewUser: React.FC = () => {
-  const {
-    toggleAddModal,
-    isAddModalOpen,
-    isViewModalOpen,
-    isEditModalOpen,
-    isDeleteModalOpen,
-    users,
-    newUser,
-    selectedUser,
-    setNewUser,
-    setSelectedUser,
-    setUsers,
-    setIsOpen,
-    isOpen,
-  } = useUserStore();
+  const { isViewModalOpen, selectedUser, toggleViewModal } = useUserStore();
   const formatDate = (date?: Date) => (date ? format(date, 'MMM dd, yyyy') : 'N/A');
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog open={isViewModalOpen} onOpenChange={toggleViewModal}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>User Details</DialogTitle>
@@ -58,11 +39,7 @@ const ViewUser: React.FC = () => {
               <div className="col-span-2">
                 <span
                   className={`px-2 py-1 rounded-full text-xs font-medium ${
-                    selectedUser.role === 'admin'
-                      ? 'bg-amber-100 text-amber-700'
-                      : selectedUser.role === 'moderator'
-                        ? 'bg-blue-100 text-blue-700'
-                        : 'bg-green-100 text-green-700'
+                    selectedUser.role === 'admin' ? 'bg-amber-100 text-amber-700' : selectedUser.role === 'moderator' ? 'bg-blue-100 text-blue-700' : 'bg-green-100 text-green-700'
                   }`}
                 >
                   {selectedUser.role}
@@ -80,10 +57,7 @@ const ViewUser: React.FC = () => {
           </div>
         )}
         <DialogFooter>
-          <Button
-            className="cursor-pointer border-1 border-slate-400 hover:border-slate-500"
-            onClick={() => setIsOpen(false)}
-          >
+          <Button className="cursor-pointer border-1 border-slate-400 hover:border-slate-500" onClick={() => toggleViewModal(false)}>
             Close
           </Button>
         </DialogFooter>
